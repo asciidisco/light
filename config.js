f113ee5f45d091ce246ab6005a08ef447107af39
Tim Park <timfpark@gmail.com>
2013-09-22 01:12:46 +0000

    nitrogen -> n2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-var n2 = require('nitrogen');
+var nitrogen = require('nitrogen');
 
 var config = {};
-config.store = new n2.FileStore(config);
+config.store = new nitrogen.FileStore(config);
 
 module.exports = config;
